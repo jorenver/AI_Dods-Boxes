98b483c4ff8c34c5e1fda8ab312e9929586d1905
obayona <obayona@espol.edu.ec>
2016-01-17 14:59:02 -0500

    Change name

--- demo.py
+++ demo.py
@@ -1,11 +1,44 @@
 import copy
 import math
-from Board import *
+from AI_Board import *
 
+def printBoard(board):
+	print "Hoizontal:"
+	print board[0]
+	print "Vertical:"
+	print board[1]
+	print "____________"
+
+def printBoard2(board):
+	
+	print "El padre"
+	print "Hoizontal:"
+	print board.horizontalEdge
+	print "Vertical:"
+	print board.verticalEdge
+	print "____________"
 
 
 if __name__ == "__main__":
-	b = Board(3,3)
+	b = AI_Board(6,6)
 	b.initBoard()
-	children = b.getChildren()
-	print len(children)
+	Temp_children = b.getChildren()
+	print "Nivel 1: numero de hijos", len(Temp_children)
+
+	cont = 0
+	print "Nivel 2, numero de hijos"
+	children = []
+	for c in Temp_children:
+		child = AI_Board(6,6)
+		child.horizontalEdge = c[0]
+		child.verticalEdge = c[1]
+		child.boxes = c[2]
+		children.append(child)
+
+	for c in children:
+		Temp_children = c.getChildren()
+		cont +=  len(Temp_children)
+	print cont
+
+
+
